388880b8385e6a18166fd920c0eab857589c14d9
tracyapps <t@thisroad.org>
2015-01-09 03:36:06 -0600

    fixing js formatting

--- theme/js/theme.js
+++ theme/js/theme.js
@@ -11,58 +11,58 @@
 
 (function( $ ){
 
-  "use strict";
+	"use strict";
 
-  $.fn.fitVids = function( options ) {
-    var settings = {
-      customSelector: null
-    };
+	$.fn.fitVids = function( options ) {
+		var settings = {
+			customSelector: null
+		};
 
-    if(!document.getElementById('fit-vids-style')) {
-      // appendStyles: https://github.com/toddmotto/fluidvids/blob/master/dist/fluidvids.js
-      var head = document.head || document.getElementsByTagName('head')[0];
-      var css = '.fluid-width-video-wrapper{width:100%;position:relative;padding:0;}.fluid-width-video-wrapper iframe,.fluid-width-video-wrapper object,.fluid-width-video-wrapper embed {position:absolute;top:0;left:0;width:100%;height:100%;}';
-      var div = document.createElement('div');
-      div.innerHTML = '<p>x</p><style id="fit-vids-style">' + css + '</style>';
-      head.appendChild(div.childNodes[1]);
-    }
+		if(!document.getElementById('fit-vids-style')) {
+			// appendStyles: https://github.com/toddmotto/fluidvids/blob/master/dist/fluidvids.js
+			var head = document.head || document.getElementsByTagName('head')[0];
+			var css = '.fluid-width-video-wrapper{width:100%;position:relative;padding:0;}.fluid-width-video-wrapper iframe,.fluid-width-video-wrapper object,.fluid-width-video-wrapper embed {position:absolute;top:0;left:0;width:100%;height:100%;}';
+			var div = document.createElement('div');
+			div.innerHTML = '<p>x</p><style id="fit-vids-style">' + css + '</style>';
+			head.appendChild(div.childNodes[1]);
+		}
 
-    if ( options ) {
-      $.extend( settings, options );
-    }
+		if ( options ) {
+			$.extend( settings, options );
+		}
 
-    return this.each(function(){
-      var selectors = [
-        "iframe[src*='player.vimeo.com']",
-        "iframe[src*='youtube.com']",
-        "iframe[src*='youtube-nocookie.com']",
-        "iframe[src*='kickstarter.com'][src*='video.html']",
-        "object",
-        "embed"
-      ];
+	return this.each(function(){
+		var selectors = [
+			"iframe[src*='player.vimeo.com']",
+			"iframe[src*='youtube.com']",
+			"iframe[src*='youtube-nocookie.com']",
+			"iframe[src*='kickstarter.com'][src*='video.html']",
+			"object",
+			"embed"
+		];
 
-      if (settings.customSelector) {
-        selectors.push(settings.customSelector);
-      }
+		if (settings.customSelector) {
+			selectors.push(settings.customSelector);
+		}
 
-      var $allVideos = $(this).find(selectors.join(','));
-      $allVideos = $allVideos.not("object object"); // SwfObj conflict patch
+		var $allVideos = $(this).find(selectors.join(','));
+		$allVideos = $allVideos.not("object object"); // SwfObj conflict patch
 
-      $allVideos.each(function(){
-        var $this = $(this);
-        if (this.tagName.toLowerCase() === 'embed' && $this.parent('object').length || $this.parent('.fluid-width-video-wrapper').length) { return; }
-        var height = ( this.tagName.toLowerCase() === 'object' || ($this.attr('height') && !isNaN(parseInt($this.attr('height'), 10))) ) ? parseInt($this.attr('height'), 10) : $this.height(),
-            width = !isNaN(parseInt($this.attr('width'), 10)) ? parseInt($this.attr('width'), 10) : $this.width(),
-            aspectRatio = height / width;
-        if(!$this.attr('id')){
-          var videoID = 'fitvid' + Math.floor(Math.random()*999999);
-          $this.attr('id', videoID);
-        }
-        $this.wrap('<div class="fluid-width-video-wrapper"></div>').parent('.fluid-width-video-wrapper').css('padding-top', (aspectRatio * 100)+"%");
-        $this.removeAttr('height').removeAttr('width');
-      });
-    });
-  };
+			$allVideos.each(function(){
+				var $this = $(this);
+				if (this.tagName.toLowerCase() === 'embed' && $this.parent('object').length || $this.parent('.fluid-width-video-wrapper').length) { return; }
+				var height = ( this.tagName.toLowerCase() === 'object' || ($this.attr('height') && !isNaN(parseInt($this.attr('height'), 10))) ) ? parseInt($this.attr('height'), 10) : $this.height(),
+				width = !isNaN(parseInt($this.attr('width'), 10)) ? parseInt($this.attr('width'), 10) : $this.width(),
+				aspectRatio = height / width;
+				if(!$this.attr('id')){
+					var videoID = 'fitvid' + Math.floor(Math.random()*999999);
+					$this.attr('id', videoID);
+				}
+				$this.wrap('<div class="fluid-width-video-wrapper"></div>').parent('.fluid-width-video-wrapper').css('padding-top', (aspectRatio * 100)+"%");
+				$this.removeAttr('height').removeAttr('width');
+			});
+		});
+	};
 // Works with either jQuery or Zepto
 })( window.jQuery || window.Zepto );
 
@@ -70,97 +70,97 @@
 * https://github.com/GaryJones/accessible-menu
 * Copyright (c) 2014 Gary Jones; Licensed MIT */
 ;(function( $, window, document, undefined ) {
-  'use strict';
+	'use strict';
 
-  var pluginName = 'gamajoAccessibleMenu',
-    hoverTimeout = [];
+	var pluginName = 'gamajoAccessibleMenu',
+	hoverTimeout = [];
 
-  // The actual plugin constructor
-  function Plugin( element, options ) {
-    this.element = element;
-    // jQuery has an extend method which merges the contents of two or
-    // more objects, storing the result in the first object. The first object
-    // is generally empty as we don't want to alter the default options for
-    // future instances of the plugin
-    this.opts = $.extend( {}, $.fn[pluginName].options, options );
-    this.init();
-  }
+	// The actual plugin constructor
+	function Plugin( element, options ) {
+		this.element = element;
+		// jQuery has an extend method which merges the contents of two or
+		// more objects, storing the result in the first object. The first object
+		// is generally empty as we don't want to alter the default options for
+		// future instances of the plugin
+		this.opts = $.extend( {}, $.fn[pluginName].options, options );
+		this.init();
+	}
 
-  // Avoid Plugin.prototype conflicts
-  $.extend( Plugin.prototype, {
-    init: function() {
-      $( this.element )
-        .on( 'mouseenter.' + pluginName, this.opts.menuItemSelector, this.opts, this.menuItemEnter )
-        .on( 'mouseleave.' + pluginName, this.opts.menuItemSelector, this.opts, this.menuItemLeave )
-        .find( 'a' )
-        .on( 'focus.'  + pluginName + ', blur.' + pluginName, this.opts, this.menuItemToggleClass );
-    },
+	// Avoid Plugin.prototype conflicts
+	$.extend( Plugin.prototype, {
+		init: function() {
+		$( this.element )
+		.on( 'mouseenter.' + pluginName, this.opts.menuItemSelector, this.opts, this.menuItemEnter )
+		.on( 'mouseleave.' + pluginName, this.opts.menuItemSelector, this.opts, this.menuItemLeave )
+		.find( 'a' )
+		.on( 'focus.'  + pluginName + ', blur.' + pluginName, this.opts, this.menuItemToggleClass );
+		},
 
-    /**
-     * Add class to menu item on hover so it can be delayed on mouseout.
-     *
-     * @since 1.0.0
-     */
-    menuItemEnter: function( event ) {
-      // Clear all existing hover delays
-      $.each( hoverTimeout, function( index ) {
-        $( '#' + index ).removeClass( event.data.hoverClass );
-        clearTimeout( hoverTimeout[index] );
-      });
-      // Reset list of hover delays
-      hoverTimeout = [];
+		/**
+		* Add class to menu item on hover so it can be delayed on mouseout.
+		*
+		* @since 1.0.0
+		*/
+		menuItemEnter: function( event ) {
+			// Clear all existing hover delays
+			$.each( hoverTimeout, function( index ) {
+				$( '#' + index ).removeClass( event.data.hoverClass );
+				clearTimeout( hoverTimeout[index] );
+			});
+			// Reset list of hover delays
+			hoverTimeout = [];
 
-      $( this ).addClass( event.data.hoverClass );
-    },
+			$( this ).addClass( event.data.hoverClass );
+		},
 
-    /**
-     * After a short delay, remove a class when mouse leaves menu item.
-     *
-     * @since 1.0.0
-     */
-    menuItemLeave: function( event ) {
-      var $self = $( this );
-      // Delay removal of class
-      hoverTimeout[this.id] = setTimeout( function() {
-        $self.removeClass( event.data.hoverClass );
-      }, event.data.hoverDelay );
-    },
+		/**
+		* After a short delay, remove a class when mouse leaves menu item.
+		*
+		* @since 1.0.0
+		*/
+		menuItemLeave: function( event ) {
+			var $self = $( this );
+			// Delay removal of class
+			hoverTimeout[this.id] = setTimeout( function() {
+				$self.removeClass( event.data.hoverClass );
+			}, event.data.hoverDelay );
+		},
 
-    /**
-     * Toggle menu item class when a link fires a focus or blur event.
-     *
-     * @since 1.0.0
-     */
-    menuItemToggleClass: function( event ) {
-      $( this ).parents( event.data.menuItemSelector ).toggleClass( event.data.hoverClass );
-    }
-  });
+		/**
+		* Toggle menu item class when a link fires a focus or blur event.
+		*
+		* @since 1.0.0
+		*/
+		menuItemToggleClass: function( event ) {
+			$( this ).parents( event.data.menuItemSelector ).toggleClass( event.data.hoverClass );
+		}
+	});
 
-  // A really lightweight plugin wrapper around the constructor,
-  // preventing against multiple instantiations
-  $.fn[ pluginName ] = function( options ) {
-    this.each( function() {
-      if ( ! $.data( this, 'plugin_' + pluginName ) ) {
-        $.data( this, 'plugin_' + pluginName, new Plugin( this, options ) );
-      }
-    });
+	// A really lightweight plugin wrapper around the constructor,
+	// preventing against multiple instantiations
+	$.fn[ pluginName ] = function( options ) {
+		this.each( function() {
+			if ( ! $.data( this, 'plugin_' + pluginName ) ) {
+				$.data( this, 'plugin_' + pluginName, new Plugin( this, options ) );
+			}
+		});
 
-    // chain jQuery functions
-    return this;
-  };
+	// chain jQuery functions
+	return this;
+	};
 
-  $.fn[ pluginName ].options = {
-    // The CSS class to add to indicate item is hovered or focused
-      hoverClass: 'menu-item-hover',
+	$.fn[ pluginName ].options = {
+		// The CSS class to add to indicate item is hovered or focused
+		hoverClass: 'menu-item-hover',
 
-      // The delay to keep submenus showing after mouse leaves
-      hoverDelay: 250,
+		// The delay to keep submenus showing after mouse leaves
+		hoverDelay: 250,
 
-      // Selector for general menu items. If you remove the default menu item
-      // classes, then you may want to call this plugin with this value
-      // set to something like 'nav li' or '.menu li'.
-      menuItemSelector: '.menu-item'
-  };
+		// Selector for general menu items. If you remove the default menu item
+		// classes, then you may want to call this plugin with this value
+		// set to something like 'nav li' or '.menu li'.
+		menuItemSelector: '.menu-item'
+	};
 })( jQuery, window, document );
 
 /*! Sidr - v1.2.1 - 2013-11-06
